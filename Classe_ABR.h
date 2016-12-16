74927ed7998338b6eba844d5eba74c5fb2fd7116
Lea Riera <lea.riera@insa-lyon.fr>
2016-12-16 17:28:15 +0100

    ABR attributes getters Constructors

---
+++ Classe_ABR.h
@@ -0,0 +1,23 @@
+class ABR {
+protected :
+  int* cle_;
+  int** fg_;
+  int** fd_;
+
+public:
+  // Getters
+  int cle();
+  ABR fg();
+  ABR fd();
+
+  // Setters
+
+  // Constructors
+  ABR();
+  ABR(int cle);
+  ABR(const ABR& model);
+
+
+  
+  
+  
