e5b2089ec3c711cf29417707f10454f0da615517
Lea Douchet <lea.douchet@insa-lyon.fr>
2016-12-16 18:02:03 +0100

    Constructeur int et par copie

--- Classe_ABR.cpp
+++ Classe_ABR.cpp
@@ -1,1 +1,13 @@
-bonjour 
+#include "Classe_ABR.h" 
+//constructors
+ABR::ABR(int cle){
+	int* cle_ = &cle;
+	int** fg_ = nullptr ;
+	int** fd_ = nullptr;
+} 
+
+ABR::ABR(const ABR& model){
+	int* cle_ = model.cle_
+	int** fg_ = model.fg_ 
+	int** fd_ = model.fd_
+}
