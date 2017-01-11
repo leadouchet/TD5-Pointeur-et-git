b0ed0b35c2e5776485666ffd29a043861647233b
Lea Riera <lea.riera@insa-lyon.fr>
2017-01-11 16:21:41 +0100

    cette fois c'est bon

--- Classe_ABR.h
+++ Classe_ABR.h
@@ -1,30 +1,20 @@
 
 class ABR {
 protected :
-  int valeur_;
   int* cle_;
   ABR* fg_;
   ABR* fd_;
 
 public:
-  // Getters
-  int* cle();
-  ABR* fg();
-  ABR* fd();
-
-  // Setters
-  void set_cle(int a);
 
   // Constructors
   ABR();
-  ABR(int a);
-  ABR(const ABR& model);
 
   //destructors
 
 
   //methodes
-bool validation();
+bool vide();
   
 ABR recherche(int nb, ABR racine);  
 };
